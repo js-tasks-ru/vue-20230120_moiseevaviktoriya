755fc2327b9b5d18a6ecc35bd44a1f36a1d4ac90
viktoriya_moiseeva <adxomega@yandex.ru>
2023-02-12 19:46:25 +0300

    SPA и vue-router

--- 05-vue-router/01-AuthPages/router/index.js
+++ 05-vue-router/01-AuthPages/router/index.js
@@ -2,5 +2,24 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 export const router = createRouter({
   history: createWebHistory('/05-vue-router/01-AuthPages'),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'PageIndex',
+      component: () => import('../views/PageIndex'),
+    },
+
+    {
+      path: '/login',
+      name: 'PageLogin',
+      component: () => import('../views/PageLogin'),
+      props: route => ({ query: route.query.from })
+    },
+
+    {
+      path: '/register',
+      name: 'PageRegister',
+      component: () => import('../views/PageRegister'),
+    }
+  ],
 });
