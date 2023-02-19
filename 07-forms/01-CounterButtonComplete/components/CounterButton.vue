aff69a86d3cd896547d37a4c6756be66e251dbf0
viktoriya_moiseeva <adxomega@yandex.ru>
2023-02-19 20:26:26 +0300

    Компоненты-формы: CounterButton, MeetupForm

--- 07-forms/01-CounterButtonComplete/components/CounterButton.vue
+++ 07-forms/01-CounterButtonComplete/components/CounterButton.vue
@@ -1,9 +1,42 @@
 <template>
-  <button></button>
+  <button @click="localCount++">{{ localCount }}</button>
 </template>
 
 <script>
 export default {
   name: 'CounterButton',
+
+  props: {
+    count: {
+      type: Number,
+      default: 0,
+    }
+  },
+
+  emits: ['update:count'],
+
+
+  data() {
+    return {
+      localCount: 0,
+    };
+  },
+
+  watch: {
+    count: {
+      immediate: true,
+      handler(newValue) {
+        if (this.localCount !== this.count) this.localCount = newValue;
+      }
+    },
+
+    localCount: {
+      handler() {
+        if (this.localCount !== this.count) {
+          this.$emit('update:count', this.localCount );
+        }
+      },
+    },
+  }
 };
 </script>
--- 07-forms/02-MeetupForm/components/MeetupForm.vue
+++ 07-forms/02-MeetupForm/components/MeetupForm.vue
@@ -1,18 +1,18 @@
 <template>
-  <form class="meetup-form">
+  <form class="meetup-form" @submit.prevent="$emit('submit', JSON.parse(JSON.stringify(this.localMeetup)))">
     <div class="meetup-form__content">
       <fieldset class="meetup-form__section">
         <UiFormGroup label="Название">
-          <UiInput name="title" />
+          <UiInput name="title" v-model="localMeetup.title"/>
         </UiFormGroup>
         <UiFormGroup label="Дата">
-          <UiInputDate type="date" name="date" />
+          <UiInputDate type="date" name="date" v-model="localMeetup.date"/>
         </UiFormGroup>
         <UiFormGroup label="Место">
-          <UiInput name="place" />
+          <UiInput name="place" v-model="localMeetup.place"/>
         </UiFormGroup>
         <UiFormGroup label="Описание">
-          <UiInput multiline rows="3" name="description" />
+          <UiInput multiline rows="3" name="description" v-model="localMeetup.description"/>
         </UiFormGroup>
         <UiFormGroup label="Изображение">
           <!--
@@ -21,24 +21,31 @@
           -->
           <ui-image-uploader
             name="image"
-            :preview="meetup.image"
-            @select="meetup.imageToUpload = $event"
-            @remove="meetup.imageToUpload = null"
+            :preview="localMeetup.image"
+            @select="localMeetup.imageToUpload = $event"
+            @remove="localMeetup.imageToUpload = null"
+            v-model="localMeetup.image"
           />
         </UiFormGroup>
       </fieldset>
 
       <h3 class="meetup-form__agenda-title">Программа</h3>
-      <!--
+
       <meetup-agenda-item-form
-         :key="agendaItem.id"
-         :agenda-item="..."
-         class="meetup-form__agenda-item"
+        v-for="(agendaItem, index) in localMeetup.agenda"
+        :key="agendaItem.id"
+        v-model:agenda-item="localMeetup.agenda[index]"
+        class="meetup-form__agenda-item"
+        @remove="removeAgendaItem(index)"
        />
-       -->
 
       <div class="meetup-form__append">
-        <button class="meetup-form__append-button" type="button" data-test="addAgendaItem">
+        <button
+          class="meetup-form__append-button"
+          type="button"
+          data-test="addAgendaItem"
+          @click="addAgendaItem"
+        >
           + Добавить этап программы
         </button>
       </div>
@@ -47,9 +54,18 @@
     <div class="meetup-form__aside">
       <div class="meetup-form__aside-stick">
         <!-- data-test атрибуты используются для поиска нужного элемента в тестах, не удаляйте их -->
-        <ui-button variant="secondary" block class="meetup-form__aside-button" data-test="cancel">Отмена</ui-button>
+        <ui-button
+          variant="secondary"
+          block
+          class="meetup-form__aside-button"
+          data-test="cancel"
+          @click="$emit('cancel')"
+        >
+          Отмена
+        </ui-button>
+
         <ui-button variant="primary" block class="meetup-form__aside-button" data-test="submit" type="submit">
-          SUBMIT
+          {{ submitText }}
         </ui-button>
       </div>
     </div>
@@ -60,10 +76,10 @@
 import MeetupAgendaItemForm from './MeetupAgendaItemForm.vue';
 import UiButton from './UiButton.vue';
 import UiFormGroup from './UiFormGroup.vue';
-import UiImageUploader from './UiImageUploader.vue';
+import UiImageUploader from '../../../06-wrappers/05-UiImageUploader/components/UiImageUploader';
 import UiInput from './UiInput.vue';
-import UiInputDate from './UiInputDate.vue';
-// import { createAgendaItem } from '../meetupService.js';
+import UiInputDate from '../../../06-wrappers/06-UiInputDate/components/UiInputDate';
+import { createAgendaItem } from '../meetupService.js';
 
 export default {
   name: 'MeetupForm',
@@ -87,6 +103,35 @@ export default {
       type: String,
       default: '',
     },
+  },
+
+  emits: ['submit', 'cancel'],
+
+  data() {
+    return {
+      localMeetup: JSON.parse(JSON.stringify(this.meetup))
+    }
+  },
+
+  methods: {
+    addAgendaItem() {
+      const newItem = createAgendaItem();
+      const agendaLength = this.localMeetup.agenda.length;
+
+      if (agendaLength) {
+        newItem.startsAt = this.localMeetup.agenda[agendaLength - 1].endsAt;
+      }
+
+      this.localMeetup.agenda.push(newItem);
+    },
+
+    removeAgendaItem(index) {
+      this.localMeetup.agenda.splice(index, 1);
+    },
+
+    handleSubmit() {
+      this.$emit('submit', this.localMeetup);
+    }
   },
 };
 </script>
