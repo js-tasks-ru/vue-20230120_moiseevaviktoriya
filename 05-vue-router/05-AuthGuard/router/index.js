df877631960f3076c4596e12b630c6345bd4ecd8
viktoriya_moiseeva <adxomega@yandex.ru>
2023-03-26 23:18:35 +0300

    SPA и vue-router UiTabsLink, AuthGuard

--- 05-vue-router/05-AuthGuard/router/index.js
+++ 05-vue-router/05-AuthGuard/router/index.js
@@ -7,10 +7,12 @@ const router = createRouter({
     {
       path: '/',
       alias: '/meetups',
+      name: 'meetups',
       component: () => import('../views/PageMeetups.vue'),
     },
     {
       path: '/login',
+      name: 'login',
       meta: {
         requireGuest: true,
       },
@@ -18,6 +20,7 @@ const router = createRouter({
     },
     {
       path: '/register',
+      name: 'register',
       meta: {
         requireGuest: true,
       },
@@ -25,6 +28,7 @@ const router = createRouter({
     },
     {
       path: '/meetups/create',
+      name: 'meetups-create',
       meta: {
         requireAuth: true,
       },
@@ -32,12 +36,21 @@ const router = createRouter({
     },
     {
       path: '/meetups/:meetupId(\\d+)/edit',
+      name: 'meetups-edit',
       meta: {
         requireAuth: true,
       },
       component: () => import('../views/PageEditMeetup.vue'),
     },
-  ],
+  ]
+});
+
+router.beforeEach(async (to) => {
+  if (to.meta.requireGuest && isAuthenticated()) {
+    return { name: 'meetups' };
+  } else if (to.meta.requireAuth && !isAuthenticated()) {
+    return { name: 'login', query: { from: to.path } }
+  }
 });
 
 export { router };
